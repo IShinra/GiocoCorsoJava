4470b779254b3f009d87e0bdfca9724489d8aad1
joshw <joshw@josh-pc>
2016-10-07 16:58:17 +0200

    attacco random

--- src/it/dstech/prova/Personaggio.java
+++ src/it/dstech/prova/Personaggio.java
@@ -7,7 +7,6 @@ public class Personaggio extends Caratteristiche implements Stili {
 	private int def;
 	private String nome;
 	private int hpTotali;
-	
 
 	public Personaggio(String nome, int stamina, int atk, int def) {
 		this.stamina = stamina;
@@ -15,23 +14,51 @@ public class Personaggio extends Caratteristiche implements Stili {
 		this.def = def;
 		this.nome = nome;
 		this.hpTotali = stamina + HPBASE;
-		
+
 	}
 
 	public static void combact(Personaggio p, Personaggio p2) {
 		boolean giocoFinito = false;
-		System.out.println("la vita massima di " + p.getNome() + " è " + p.getHpTotali() );
-		System.out.println("la vita massima di " + p2.getNome() + " è " + p2.getHpTotali() );
-		while (giocoFinito == false){
-			int x=p2.pugno(p2);
-			int y=p.calcio(p);
-			System.out.println("Naruto usa pugno potenza pari a: " + x );
+		System.out.println("la vita massima di " + p.getNome() + " è " + p.getHpTotali());
+		System.out.println("la vita massima di " + p2.getNome() + " è " + p2.getHpTotali());
+		while (giocoFinito == false) {
+			int numcasuale = (int) (0 + Math.random() * 3);
+			int x = p.calcio(p);
+			int y = p.pugno(p);
+			switch (numcasuale) {
+			case 0:
+				x = p.pugno(p);
+				System.out.println("Naruto usa  pugno potenza pari a: " + x);
+				break;
+			case 1:
+				x = p.calcio(p);
+				System.out.println("Naruto usa  calcio potenza pari a: " + x);
+				break;
+			case 2:
+				x = p.atkspc(p);
+				System.out.println("Naruto usa attacco speciale potenza pari a: " + x);
+				break;
+			}
 			p.setHpTotali(p.hpTotali - x);
-			System.out.println("vita rimasta a Goku : " + p.getHpTotali() );
-			
-			System.out.println("Goku usa calcio potenza pari a: " + y);
+			System.out.println("vita rimasta a Goku : " + p.getHpTotali());
+
+			numcasuale = (int) (0 + Math.random() * 3);
+			switch (numcasuale) {
+			case 0:
+				y = p2.pugno(p2);
+				System.out.println("Goku usa pugno potenza pari a: " + y);
+				break;
+			case 1:
+				y = p2.calcio(p2);
+				System.out.println("Goku usa calcio potenza pari a: " + y);
+				break;
+			case 2:
+				y = p2.atkspc(p2);
+				System.out.println("Goku usa attacco speciale potenza pari a: " + y);
+				break;
+			}
 			p2.setHpTotali(p2.hpTotali - y);
-			System.out.println("vita rimasta a Naruto : " + p2.getHpTotali() );
+			System.out.println("vita rimasta a Naruto : " + p2.getHpTotali());
 
 			if (p.getHpTotali() <= 0 || p2.getHpTotali() <= 0) {
 
@@ -40,13 +67,13 @@ public class Personaggio extends Caratteristiche implements Stili {
 					giocoFinito = true;
 				} else {
 					System.out.println("il vincitore è: " + p.getNome());
-					giocoFinito = true; 
+					giocoFinito = true;
 				}
 			} else if (p.getHpTotali() == 0 && p.getHpTotali() == 0) {
 				System.out.println("Pareggio");
-				giocoFinito = true; 
+				giocoFinito = true;
 			}
-		} 
+		}
 
 	}
 
@@ -59,7 +86,7 @@ public class Personaggio extends Caratteristiche implements Stili {
 	 */
 
 	public int getHpTotali() {
-		
+
 		return hpTotali;
 	}
 
@@ -87,7 +114,7 @@ public class Personaggio extends Caratteristiche implements Stili {
 	public int pugno(Personaggio p) {
 		int danno;
 		danno = (p.randomCritico(p) + 50);
-		danno -= getDEF();		
+		danno -= getDEF();
 		return danno;
 	}
 
@@ -95,15 +122,15 @@ public class Personaggio extends Caratteristiche implements Stili {
 	public int calcio(Personaggio p) {
 		int danno;
 		danno = (p.randomCritico(p) + 70);
-		danno -= getDEF();	
+		danno -= getDEF();
 		return danno;
 	}
 
 	@Override
 	public int atkspc(Personaggio p) {
 		int danno;
-		danno = (p.randomCritico(p) + 10);
-		danno -= getDEF();	
+		danno = (p.randomCritico(p) + 100);
+		danno -= getDEF();
 		return danno;
 	}
 
