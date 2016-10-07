1241bb426d89aefd973313778f26907aa6f51e13
Federico <Federico@LAPTOP-90KS67BP>
2016-10-07 16:32:44 +0200

    w la figa

--- src/it/dstech/prova/Game.java
+++ src/it/dstech/prova/Game.java
@@ -3,23 +3,26 @@ package it.dstech.prova;
 public class Game {
 	public static void main(String args[]) {
 
-		Personaggio goku = new Personaggio("Goku", 10, 10, 10);
-		Personaggio naruto = new Personaggio("Vegeta", 5, 5, 5);
+		Personaggio goku = new Personaggio("Goku", 1000, 10, 10);
+		Personaggio naruto = new Personaggio("Naruto",1000,10, 10);
 
 		/*
 		 * pg.cammina(); pg.pugno(); System.out.println("HP ; HPBASE " +
 		 * pg.HPBASE + " - " + pg.getHP()); System.out.println("DEF ; DEFBASE "
-		 * + pg.DEFBASE + " - " + pg.getDEF());
-		 * System.out.println("ATK ; ATKBASE " + pg.ATKBASE + " - " +
-		 * pg.getATK()); System.out.println("DANNO ATTACCO SPECIALE " +
-		 * pg.khamehameha()); System.out.println( "HAI SUBITO UN DANNO DI " +
-		 * pg.khamehameha() + ",ORA HAI " + pg.setHP(pg.khamehameha()) + "HP");
+		 * + pg.DEFBASE + " - " + pg.getDEF()); System.out.println(
+		 * "ATK ; ATKBASE " + pg.ATKBASE + " - " + pg.getATK());
+		 * System.out.println("DANNO ATTACCO SPECIALE " + pg.khamehameha());
+		 * System.out.println( "HAI SUBITO UN DANNO DI " + pg.khamehameha() +
+		 * ",ORA HAI " + pg.setHP(pg.khamehameha()) + "HP");
 		 */
 
-		System.out.println("Goku's HP : " + goku.getHP());
-		System.out.println("Naruto's HP : " + naruto.getHP());
-		System.out.println("Goku engage Naruto. Naruto has : " + naruto.setHP(goku.getATK()));
-		System.out.println("Naruto engage Goku. Naruto has : " + goku.setHP(naruto.getATK()));
+		/*
+		 * System.out.println("Goku's HP : " + goku.getHP());
+		 * System.out.println("Naruto's HP : " + naruto.getHP());
+		 * System.out.println("Goku engage Naruto. Naruto has : " +
+		 * naruto.setHP(goku.getATK())); System.out.println(
+		 * "Naruto engage Goku. Naruto has : " + goku.setHP(naruto.getATK()));
+		 */
 
 		Personaggio.combact(goku, naruto);
 	}
--- src/it/dstech/prova/Personaggio.java
+++ src/it/dstech/prova/Personaggio.java
@@ -1,35 +1,70 @@
 package it.dstech.prova;
 
 public class Personaggio extends Caratteristiche implements Stili {
+
 	private int stamina;
 	private int atk;
 	private int def;
 	private String nome;
+	private int hpTotali;
+	
 
 	public Personaggio(String nome, int stamina, int atk, int def) {
 		this.stamina = stamina;
 		this.atk = atk;
 		this.def = def;
 		this.nome = nome;
+		this.hpTotali = stamina + HPBASE;
+		
 	}
 
 	public static void combact(Personaggio p, Personaggio p2) {
-		System.out.println("Goku : " + p.setHP(p2.pugno(p2) - p.getDEF()));
-		System.out.println("Naruto : " + p2.setHP(p.calcio(p) - p2.getDEF()));
+		boolean giocoFinito = false;
+		System.out.println("la vita massima di " + p.getNome() + " è " + p.getHpTotali() );
+		System.out.println("la vita massima di " + p2.getNome() + " è " + p2.getHpTotali() );
+		while (giocoFinito == false){
+			int x=p2.pugno(p2);
+			int y=p.calcio(p);
+			System.out.println("Naruto usa pugno potenza pari a: " + x );
+			p.setHpTotali(p.hpTotali - x);
+			System.out.println("vita rimasta a Goku : " + p.getHpTotali() );
+			
+			System.out.println("Goku usa calcio potenza pari a: " + y);
+			p2.setHpTotali(p2.hpTotali - y);
+			System.out.println("vita rimasta a Naruto : " + p2.getHpTotali() );
 
-		if (p.getHP() > p2.getHP())
-			System.out.println("Vincitore : " + p.getNome());
+			if (p.getHpTotali() <= 0 || p2.getHpTotali() <= 0) {
 
-		else
-			System.out.println("Vincitore : " + p2.getNome());
+				if (p.getHpTotali() < p2.getHpTotali()) {
+					System.out.println("il vincitore è: " + p2.getNome());
+					giocoFinito = true;
+				} else {
+					System.out.println("il vincitore è: " + p.getNome());
+					giocoFinito = true; 
+				}
+			} else if (p.getHpTotali() == 0 && p.getHpTotali() == 0) {
+				System.out.println("Pareggio");
+				giocoFinito = true; 
+			}
+		} 
+
 	}
 
-	public int getHP() {
-		return (HPBASE + stamina);
+	/*
+	 * public int getHP() { return (HPBASE + stamina); }
+	 * 
+	 * public void setHP(int danno) { getHP() -= danno;
+	 * 
+	 * }
+	 */
+
+	public int getHpTotali() {
+		
+		return hpTotali;
 	}
 
-	public int setHP(int danno) {
-		return (getHP() - danno);
+	public void setHpTotali(int hpTotali) {
+		this.hpTotali = hpTotali;
 	}
 
 	public int getATK() {
@@ -51,22 +86,33 @@ public class Personaggio extends Caratteristiche implements Stili {
 	@Override
 	public int pugno(Personaggio p) {
 		int danno;
-		danno = (p.getATK() + 50);
+		danno = (p.randomCritico(p) + 50);
+		danno -= getDEF();		
 		return danno;
 	}
 
 	@Override
 	public int calcio(Personaggio p) {
 		int danno;
-		danno = (p.getATK() + 70);
+		danno = (p.randomCritico(p) + 70);
+		danno -= getDEF();	
 		return danno;
 	}
 
 	@Override
 	public int atkspc(Personaggio p) {
 		int danno;
-		danno = (p.getATK() + 100);
+		danno = (p.randomCritico(p) + 10);
+		danno -= getDEF();	
 		return danno;
+	}
+
+	public int randomCritico(Personaggio p) {
+		int attaccoMax = p.getATK();
+		int range = (int) (0 + Math.random() * attaccoMax);
+		if (range == attaccoMax)
+			System.out.println("che culo hai fatto un colpo di cristo(critico)!!!:)");
+		return range;
 	}
 
 }
